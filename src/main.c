364c7ae7b0b418f32b5d277f8c4bd934a5dd49b3
Truls Rustad Fossum <trulfos@gmail.com>
2016-03-02 15:06:03 +0100

    Almost correct results

--- src/main.c
+++ src/main.c
@@ -4,19 +4,23 @@
 
 int main(int argv, char ** argc) {
 
-    int nTemperatures = 10;
-    double endTime = 0.01;
-    double dt = 0.001;
-    double s = 1.0;
+    int nTemperatures = 80;
+    double t = 0.1;
+    double endTime = 0.5;
+    double dt = 0.0000001;
+    double s = t;
     double ds = 0.0;
-    double initialTemperature = 1.0;
+    //double initialTemperature = 1.0;
 
     // Create an fill array of temperatures
     double * temperatures = malloc(sizeof(double) * nTemperatures);
 
-    for (int i = 0; i < nTemperatures; i++) {
-        temperatures[i] = initialTemperature;
+    for (int i = 0; i < nTemperatures - 1; i++) {
+        //temperatures[i] = initialTemperature;
+        temperatures[i] = exp(t - i * s / nTemperatures) - 1;
     }
+
+    temperatures[nTemperatures - 1] = 0.0;
 
     // Print header for csv output
     printf("t");
@@ -26,7 +30,7 @@ int main(int argv, char ** argc) {
     printf(", s\n");
 
     // Step forward in time
-    for (double t = 0.0; t < endTime; t += dt) {
+    while (t < endTime) {
         double h = s / nTemperatures;
         double r = dt / (h * h);
 
@@ -34,15 +38,15 @@ int main(int argv, char ** argc) {
         double prev_u = temperatures[0];
 
         // Boundary
-        temperatures[0] = (1 - 2 * r) + 2 * r * temperatures[1]
+        temperatures[0] = (1 - 2 * r) * temperatures[0] + 2 * r * temperatures[1]
                 + (2 * r * h) * exp(t);
 
 
         // Interior
-        for (int i = 1; i < nTemperatures; i++) {
+        for (int i = 1; i < nTemperatures - 1; i++) {
 
             double du = ((temperatures[i + 1] - prev_u) * dt * ds * i)
-                    / (2 * s * dt)
+                    / (2 * dt * h * nTemperatures)
                 + (
                     temperatures[i + 1]
                     - 2 * temperatures[i]
@@ -57,12 +61,12 @@ int main(int argv, char ** argc) {
 
         // Update boundary position
         double ds = -(
-                    dt * nTemperatures * (
+                    dt * (
                         3 * temperatures[nTemperatures - 1]
                         -4 * temperatures[nTemperatures - 2]
                        + temperatures[nTemperatures - 3]
                     )
-                ) / (2 * s);
+                ) / (2 * h);
 
         s += ds;
 
@@ -71,13 +75,14 @@ int main(int argv, char ** argc) {
             fprintf(stderr, "WARNING: Condition for stability not fulfilled.\n");
         }
 
+        t += dt;
+    }
         // Print temperatures
         printf("%f", t);
         for (int i = 0; i < nTemperatures; i++) {
             printf(", %f", temperatures[i]);
         }
         printf(", %f\n", s);
-    }
 
     // Clean up and return
     free(temperatures);
